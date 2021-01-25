74d8915f821b7b9387218012a294565a58e870e4
CrossNox <ijmermet@gmail.com>
2021-01-25 17:56:45 -0300

    install ssm plugin for aws cli

--- setup_aws_cli_v2.sh
+++ setup_aws_cli_v2.sh
@@ -2,3 +2,6 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 complete -C aws_completer aws
+curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/linux_64bit/session-manager-plugin.rpm" -o "session-manager-plugin.rpm"
+sudo dnf install -y session-manager-plugin.rpm
+rm session-manager-plugin.rpm

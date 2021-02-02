7aa03c3383ea6dd2029b129dea5459bc710674d0
CrossNox <ijmermet@gmail.com>
2021-02-02 17:27:52 -0300

    update terraform

--- install_terraform.sh
+++ install_terraform.sh
@@ -1,5 +1,5 @@
-mkdir ~/bin
+mkdir -p ~/bin
 cd ~/bin
-wget https://releases.hashicorp.com/terraform/0.12.26/terraform_0.12.26_linux_amd64.zip
-unzip terraform_0.12.26_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/0.14.5/terraform_0.14.5_linux_amd64.zip
+unzip terraform_0.14.5_linux_amd64.zip
 rm terraform*zip

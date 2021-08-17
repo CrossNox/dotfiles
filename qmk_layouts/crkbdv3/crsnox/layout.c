123bf0abca3d961be859250c34c1bae677d417e0
CrossNox <ijmermet@gmail.com>
2021-08-17 12:41:11 -0300

    yet more changes

--- qmk_layouts/crkbdv3/crsnox/layout.c
+++ qmk_layouts/crkbdv3/crsnox/layout.c
@@ -11,7 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                              KC_I, KC_O, KC_P, KC_GRV, KC_LSFT, KC_A, KC_S,
                              KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN,
                              KC_TAB, KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B,
-                             KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_QUOT,
+                             KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_EQL,
                              KC_LGUI, MO(1), KC_SPC, KC_ENT, MO(2), KC_BSPC),
     [1] = LAYOUT_split_3x6_3(KC_LALT, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7,
                              KC_8, KC_9, KC_0, KC_GRV, KC_LSFT, KC_NO, KC_UP,
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                              KC_RGHT, KC_HOME, KC_TAB, KC_LCTL, KC_LEFT,
                              KC_DOWN, KC_RGHT, KC_MINS, KC_LBRC, KC_RBRC,
                              KC_BSLS, KC_PGUP, KC_PGDN, KC_END, KC_DEL, KC_LGUI,
-                             KC_TRNS, KC_NO, KC_NO, MO(3), OSM(MOD_RALT)),
+                             KC_TRNS, KC_NO, KC_QUOT, MO(3), OSM(MOD_RALT)),
     [2] = LAYOUT_split_3x6_3(
         KC_ESC, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR,
         KC_ASTR, KC_LPRN, KC_RPRN, KC_PSCR, KC_LSFT, KC_NO, KC_UP, KC_NO, KC_NO,

82aa9376f11d0b0a2193eb5f63c64130665d7027
CrossNox <ijmermet@gmail.com>
2021-09-10 15:21:09 -0300

    some rgb

--- qmk_layouts/crkbdv3/crsnox/keymap.c
+++ qmk_layouts/crkbdv3/crsnox/keymap.c
@@ -55,46 +55,16 @@ void oled_render_layer_state(void) {
   }
 }
 
-// char keylog_str[24] = {};
-
-// const char code_to_name[60] = {
-//     ' ', ' ',  ' ', ' ', 'a',  'b', 'c', 'd', 'e', 'f', 'g', 'h',
-//     'i', 'j',  'k', 'l', 'm',  'n', 'o', 'p', 'q', 'r', 's', 't',
-//     'u', 'v',  'w', 'x', 'y',  'z', '1', '2', '3', '4', '5', '6',
-//     '7', '8',  '9', '0', 'R',  'E', 'B', 'T', '_', '-', '=', '[',
-//     ']', '\\', '#', ';', '\'', '`', ',', '.', '/', ' ', ' ', ' '};
-//
-// void set_keylog(uint16_t keycode, keyrecord_t *record) {
-//   char name = ' ';
-//   if ((keycode >= QK_MOD_TAP && keycode <= QK_MOD_TAP_MAX) ||
-//       (keycode >= QK_LAYER_TAP && keycode <= QK_LAYER_TAP_MAX)) {
-//     keycode = keycode & 0xFF;
-//   }
-//   if (keycode < 60) {
-//     name = code_to_name[keycode];
-//   }
-//
-//   // update keylog
-//   snprintf(keylog_str, sizeof(keylog_str), "%dx%d, k%2d : %c",
-//            record->event.key.row, record->event.key.col, keycode, name);
-// }
-
-// void oled_render_keylog(void) { oled_write(keylog_str, false); }
-
-// void render_bootmagic_status(bool status) {
-//   /* Show Ctrl-Gui Swap options */
-//   static const char PROGMEM logo[][2][3] = {
-//       {{0x97, 0x98, 0}, {0xb7, 0xb8, 0}},
-//       {{0x95, 0x96, 0}, {0xb5, 0xb6, 0}},
-//   };
-//   if (status) {
-//     oled_write_ln_P(logo[0][0], false);
-//     oled_write_ln_P(logo[0][1], false);
-//   } else {
-//     oled_write_ln_P(logo[1][0], false);
-//     oled_write_ln_P(logo[1][1], false);
-//   }
-// }
+static void render_rgbmatrix_status(bool full) {
+#ifdef RGB_MATRIX_ENABLE
+  char buf[30];
+  if (rgb_matrix_is_enabled()) {
+    snprintf(buf, sizeof(buf), "RGB %2d: %d, %d, %d ", rgblight_get_mode(),
+             rgblight_get_hue(), rgblight_get_sat(), rgblight_get_val());
+    oled_write_ln(buf, false);
+  }
+#endif
+}
 
 void oled_render_logo(void) {
   static const char PROGMEM crkbd_logo[] = {
@@ -110,16 +80,10 @@ void oled_render_logo(void) {
 void oled_task_user(void) {
   if (is_keyboard_master()) {
     oled_render_layer_state();
-    // oled_render_keylog();
+    render_rgbmatrix_status(true);
   } else {
     oled_render_logo();
   }
 }
 
-// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-//   if (record->event.pressed) {
-//     set_keylog(keycode, record);
-//   }
-//   return true;
-// }
 #endif // OLED_DRIVER_ENABLE

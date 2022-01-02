10b0fc6cfec4be41b53f468ffa6593c06d2f3e4b
CrossNox <ijmermet@gmail.com>
2022-01-02 18:51:15 -0300

    fonts fix

--- fedora/fedora_setup.sh
+++ fedora/fedora_setup.sh
@@ -160,12 +160,10 @@ pip3 install --user jedi
 
 # fonts
 echo "Downloading fonts"
-mkdir -p /.local/share/fonts
-
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts
 curl -fLo "Droid Sans Mono Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
-
 curl -fLo "Fira Code Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Regular/complete/Fira%20Code%20Regular%20Nerd%20Font%20Complete.ttf
-
 fc-cache -v
 
 # NB

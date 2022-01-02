1dd472984fc4241df0f32579d8949e22f8187897
CrossNox <ijmermet@gmail.com>
2022-01-02 14:42:37 -0300

    overwrite ln if exists

--- fedora/fedora_setup.sh
+++ fedora/fedora_setup.sh
@@ -91,7 +91,7 @@ rvm --default use 2.7.0
 echo "Installing kitty"
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 mkdir -p ~/.local/bin
-ln -s ~/.local/kitty.app/bin/kitty ~/.local/bin/
+ln -sf ~/.local/kitty.app/bin/kitty ~/.local/bin/
 cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications
 sed -i "s/Icon\=kitty/Icon\=\/home\/$USER\/.local\/kitty.app\/share\/icons\/hicolor\/256x256\/apps\/kitty.png/g" ~/.local/share/applications/kitty.desktop
 

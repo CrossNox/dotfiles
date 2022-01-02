ecc2db2053e6b799ef3efe2c06f6effe547755f3
CrossNox <ijmermet@gmail.com>
2022-01-02 14:25:35 -0300

    create bin folder if not exists

--- fedora/fedora_setup.sh
+++ fedora/fedora_setup.sh
@@ -90,6 +90,7 @@ rvm --default use 2.7.0
 # kitty
 echo "Installing kitty"
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+mkdir -p ~/.local/bin
 ln -s ~/.local/kitty.app/bin/kitty ~/.local/bin/
 cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications
 sed -i "s/Icon\=kitty/Icon\=\/home\/$USER\/.local\/kitty.app\/share\/icons\/hicolor\/256x256\/apps\/kitty.png/g" ~/.local/share/applications/kitty.desktop

08f5c4aeb72bd1279a6fc01282ffb988cb5045ef
CrossNox <ijmermet@gmail.com>
2022-01-02 09:25:20 -0300

    repos folder as sudo user

--- fedora/fedora_setup.sh
+++ fedora/fedora_setup.sh
@@ -19,15 +19,19 @@ DOTFILES_FOLDER=$SUDO_USER_HOME/repos/dotfiles
 echo "Cloning repo"
 
 if [ ! -d "$REPOS_FOLDER" ] ; then
+	su $SUDO_USER
     mkdir -p $REPOS_FOLDER
+	exit
 fi
 
 if [ ! -d "$REPOS_FOLDER/dotfiles" ] ; then
+	su $SUDO_USER
     git clone https://github.com/CrossNox/dotfiles.git $DOTFILES_FOLDER
+	exit
 fi
 
 cd $DOTFILES_FOLDER
-git pull
+su $SUDO_USER -c "git pull"
 
 
 # set dnf repos

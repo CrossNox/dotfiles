eb2482d5e62e5b5aef768ec3a9d8f44022eb027e
CrossNox <ijmermet@gmail.com>
2022-02-09 22:24:45 -0300

    fix rofi power

--- .config/polybar/scripts/rofi-poweroff.sh
+++ .config/polybar/scripts/rofi-poweroff.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-chosen=$(printf "\tLog out\n\tPower Off\n\tRestart\n\tLock" | rofi -dmenu -i -theme-str '@import "power.rasi"')
+chosen=$(printf "  Power Off\n  Restart\n  Lock" | rofi -dmenu -i -theme-str '@import "power.rasi"')
 
 case "$chosen" in
-	"\tLog out" ) bspc quit ;;
-	"\tPower Off") systemctl poweroff ;;
-	"\tRestart") systemctl reboot ;;
-	"\tLock") slock ;;
+	# "\tLog out" ) bspc quit ;;
+	"  Power Off") systemctl poweroff ;;
+	"  Restart") systemctl reboot ;;
+	"  Lock") slock ;;
 	*) exit 1 ;;
 esac

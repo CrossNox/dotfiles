a2d2473dcab9b7bbf67d67690bad0e0d1f36f658
CrossNox <ijmermet@gmail.com>
2022-02-24 05:11:28 -0300

    bottom bar

--- home/.config/polybar/scripts/launch.sh
+++ home/.config/polybar/scripts/launch.sh
@@ -12,8 +12,12 @@ echo "Launching polybar"
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
 	  POLYBAR_GH_ACCESS_TOKEN=$(pass github/tokens/polybar) MONITOR=$m polybar --reload --config=$HOME/.config/polybar/statusbar.ini statusbar &
+	  POLYBAR_GH_ACCESS_TOKEN=$(pass github/tokens/polybar) MONITOR=$m polybar --reload --config=$HOME/.config/polybar/spotify.ini spotibar &
+	  POLYBAR_GH_ACCESS_TOKEN=$(pass github/tokens/polybar) MONITOR=$m polybar --reload --config=$HOME/.config/polybar/bottom.ini bottomstatus &
   done
 else
-  polybar --reload statusbar --config $HOME/.config/polybar/statusbar.ini &
+	POLYBAR_GH_ACCESS_TOKEN=$(pass github/tokens/polybar) polybar --reload --config=$HOME/.config/polybar/statusbar.ini statusbar &
+	POLYBAR_GH_ACCESS_TOKEN=$(pass github/tokens/polybar) polybar --reload --config=$HOME/.config/polybar/spotify.ini spotibar &
+	POLYBAR_GH_ACCESS_TOKEN=$(pass github/tokens/polybar) polybar --reload --config=$HOME/.config/polybar/bottom.ini bottomstatus &
 fi
 echo "Polybar launched"

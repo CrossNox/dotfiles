da07ea9cd412ecc71fc35d7dc86a9031bd99d0c6
CrossNox <ijmermet@gmail.com>
2023-06-23 18:21:29 -0300

    fix

--- fedora/fedora_setup.sh
+++ fedora/fedora_setup.sh
@@ -134,7 +134,7 @@ curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
 curl -sSL https://rvm.io/pkuczynski.asc | gpg2 --import -
 curl -L get.rvm.io | bash -s stable
 # echo 'source ~/.rvm/scripts/rvm' >> ~/.bashrc
-source $HOME/.bashrc
+source $SUDO_USER_HOME/.bashrc
 rvm install 2.7.0
 rvm --default use 2.7.0
 

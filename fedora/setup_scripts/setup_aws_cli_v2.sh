865f262457d9db33f3c4e5431d5229bd94bbbfe0
CrossNox <ijmermet@gmail.com>
2023-06-23 17:21:09 -0300

    fix

--- fedora/setup_scripts/setup_aws_cli_v2.sh
+++ fedora/setup_scripts/setup_aws_cli_v2.sh
@@ -4,7 +4,7 @@ echo "Installing AWS CLI v2"
 cd /tmp
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip -q awscliv2.zip
-./aws/install
+sudo ./aws/install
 complete -C aws_completer aws
 curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/linux_64bit/session-manager-plugin.rpm" -o "session-manager-plugin.rpm"
 dnf install -y session-manager-plugin.rpm

27fa55b93fca439ed9639ce3c47de0ca6b9119b8
CrossNox <ijmermet@gmail.com>
2023-06-25 21:20:40 -0300

    rsa? ew

--- fedora/postinstall/ssh_key.sh
+++ fedora/postinstall/ssh_key.sh
@@ -1,3 +1,3 @@
-ssh-keygen -t rsa -b 4096 -C $1 -N $2 -f ~/.ssh/id_rsa.pub
-xclip -sel clip < ~/.ssh/id_rsa.pub
+ssh-keygen -t ed25519 -C $1 -f ~/.ssh/id_ed25519.pub
+xclip -sel clip <~/.ssh/id_ed25519.pub
 read -p "Add ssh key to github. Then press enter"

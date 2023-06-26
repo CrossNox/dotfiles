5e8aa5f6c4ebf8a30405af336042158b64d5671c
CrossNox <ijmermet@gmail.com>
2023-06-26 01:44:43 -0300

    fix

--- fedora/postinstall/ssh_key.sh
+++ fedora/postinstall/ssh_key.sh
@@ -1,3 +1,5 @@
-ssh-keygen -t ed25519 -C $1 -f ~/.ssh/id_ed25519.pub
+ssh-keygen -t ed25519 -C $1 -f ~/.ssh/id_ed25519
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
 xclip -sel clip <~/.ssh/id_ed25519.pub
 read -p "Add ssh key to github. Then press enter"

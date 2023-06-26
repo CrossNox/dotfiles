5bfb7b458e6b082705f1356ef26cb34af8d769f3
CrossNox <ijmermet@gmail.com>
2023-06-25 23:13:08 -0300

    postinstall

---
+++ fedora/postinstall/init_services.sh
@@ -0,0 +1,4 @@
+systemctl --user start xautolock
+systemctl --user start gmail-notifications-uni.service
+systemctl --user start gmail-notifications-work.service
+systemctl --user start gmail-notifications-personal.service

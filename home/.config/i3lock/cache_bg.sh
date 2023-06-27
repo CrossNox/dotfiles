1bead7b9f15b47dc933b8f89b772942af389e643
CrossNox <ijmermet@gmail.com>
2023-06-27 14:21:37 -0300

    fix lock screen

--- home/.config/i3lock/cache_bg.sh
+++ home/.config/i3lock/cache_bg.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
+set -e -o functrace
+trap 'failure "LINENO" "BASH_LINENO" "#{BASH_COMMAND}" "${?}"' ERR
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+
 WP=$1
 ICON=$HOME/.config/i3lock/resources/icon.png
 TMPBG=$HOME/.config/i3lock/resources/lock.png
 
-convert "$WP" -blur 0x12 -resize $(xrandr --query | grep primary | grep -oP '\d+x\d+') "$TMPBG"
+RES=$(xrandr --query | grep primary | grep -oP '\d+x\d+')
 
-convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
+echo "Converting $WP to $TMPBG with resolution $RES"
+/usr/bin/convert "$WP" -blur 0x12 -background none -gravity center -extent "$RES" -resize "$RES" -trim "$TMPBG"
+
+echo "Composite background"
+/usr/bin/convert "$TMPBG" "$ICON" -gravity center -composite -matte "$TMPBG"

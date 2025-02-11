97cc86403844e34bfa0892b49fea7100dbfa0554
CrossNox <ijmermet@gmail.com>
2025-02-11 12:12:03 -0300

    fix

--- 3dprinters/Configuration.h
+++ 3dprinters/Configuration.h
@@ -2025,7 +2025,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 7
+  #define GRID_MAX_POINTS_X 5
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
